dfd8d22fdfe8d1734692a906646cdd75344c4d52
philipportner <philipp.ortner@student.tugraz.at>
2020-09-27 21:17:38 +0200

    enable vterm, change "s" to avy-got-char-2

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -26,7 +26,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-molokai)
+(setq doom-theme 'doom-gruvbox)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -53,3 +53,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(after! evil-snipe
+  (evil-snipe-mode -1))
+(map! :n "s" #'avy-goto-char-2)
