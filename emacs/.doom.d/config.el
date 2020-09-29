07e2830ded648ea4a2ad8ad5b0e7369c88979b25
philipportner <philipp.ortner@student.tugraz.at>
2020-09-29 22:32:01 +0200

    emacs - font size and ivy +fuzzy and dired +ranger

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -20,8 +20,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Jetbrains Mono" :size 16 :weight 'medium)
-      doom-variable-pitch-font (font-spec :family "Jetbrains Mono" :size 16 :weight 'medium))
+(setq doom-font (font-spec :family "Jetbrains Mono Medium" :size 20 :weight 'medium)
+      doom-variable-pitch-font (font-spec :family "Jetbrains Mono Medium" :size 20 :weight 'medium))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -23,13 +23,13 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +fuzzy)               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;; doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
@@ -65,7 +65,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +ranger)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
